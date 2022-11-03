b0e935a38a67c773b8188ce75475a31753476252
Sophia Pache <sophpache@gmail.com>
2022-11-03 13:38:19 +1100

    new migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_02_015908) do
+ActiveRecord::Schema.define(version: 2022_11_03_023702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2022_11_02_015908) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
   end
 
   create_table "venues", force: :cascade do |t|
