2ec31706d4e1c2030d5a7d25b03ffa79637ada1b
Sophia Pache <sophpache@gmail.com>
2022-11-08 09:42:39 +1100

    added comments

--- config/initializers/spotify.rb
+++ config/initializers/spotify.rb
@@ -1,2 +1,2 @@
-RSpotify::authenticate("1faf75c1ce434427a6eddce1aa3eac1a", "b8077ddfca79472eae08b4c68602ea2d")
+RSpotify::authenticate(Rails.application.secrets.spotify_client_id, Rails.application.secrets.spotify_api_key)
 
